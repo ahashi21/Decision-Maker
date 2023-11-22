a8eb6ece91ae4eea31f9258a9920b155eb497ac0
Todd <todd.fountain39@gmail.com>
2023-11-22 14:23:34 -0600

    schema creation

--- db/schema/04_votes.sql
+++ db/schema/04_votes.sql
@@ -1,11 +1,10 @@
--- Drop and recreate Widgets table (Example)
-
 CREATE TABLE votes (
   id SERIAL PRIMARY KEY NOT NULL,
+  user_id INTEGER NOT NULL,
   poll_id INTEGER NOT NULL,
   choice_id INTEGER NOT NULL,
+  rank INTEGER NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users(id) ON DELETE CASCADE,
   FOREIGN KEY (poll_id) REFERENCES polls(id) ON DELETE CASCADE,
-  FOREIGN KEY (choice_id) REFERENCES choices(id) ON DELETE CASCADE,
-  UNIQUE (poll_id, choice_id)
+  FOREIGN KEY (choice_id) REFERENCES choices(id) ON DELETE CASCADE
 );
