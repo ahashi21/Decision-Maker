80d4325dc4fe480ec39254ea99fc720b21b8a808
Aden <aden.mccarville@gmail.com>
2023-11-22 19:37:09 -0500

    Create knexfile

---
+++ knexfile.js
@@ -0,0 +1,24 @@
+require('dotenv').config();
+
+module.exports = {
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      port: process.env.DB_PORT
+    },
+    seeds: {
+      directory: './db/seeds',
+    },
+  },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    seeds: {
+      directory: './db/seeds',
+    },
+  },
+};
