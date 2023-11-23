7a787c0e6cb46a963a660968e1b5d0bedfd1d4db
Aden <aden.mccarville@gmail.com>
2023-11-23 17:42:19 -0500

    Update new-poll-submit with Mailgun integration

--- routes/new-poll-submit.js
+++ routes/new-poll-submit.js
@@ -1,12 +1,38 @@
 const express = require('express');
 const router = express.Router();
 const PollHelper = require('../helpers/poll-helper');
+const mailgun = require('mailgun-js');
+
+// Mailgun configuration
+const mg = mailgun({
+  apiKey: '49e18c89f7fd32422d9adc3334aaeeaa-5d2b1caa-82b398c5',
+  domain: 'sandbox87dba3e3ffa04898897c8a0868b7285b.mailgun.org'
+});
+
+// Send an email using Mailgun
+function sendEmail(to, subject, text) {
+  const data = {
+    from: 'decision-maker@sandbox87dba3e3ffa04898897c8a0868b7285b.mailgun.org',
+    to,
+    subject,
+    text,
+  };
+
+  mg.messages().send(data, (error, body) => {
+    if (error) {
+      console.error(error);
+    } else {
+      console.log(body);
+    }
+  });
+}
 
 router.post('/polls', async (req, res) => {
   try {
     const { email, choices } = req.body;
     const { adminLink, userLink } = await PollHelper.createPoll(email, choices);
-    res.redirect('/'); // To be replaced with mailgun integration and redirect to "success" page
+    sendEmail(email, 'Your poll has been created!', `Send this link to your friends: ${userLink}\nCheck the results here: ${adminLink}`);
+    res.redirect('/'); // To be replaced with redirection to "success" page
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'Error!' });
